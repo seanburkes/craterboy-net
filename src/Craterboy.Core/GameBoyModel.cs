10f1d0c10ce5aa9bfab793de322ccd17926fe034
Sean Burkes <sean.burkes@gmail.com>
2026-08-09 19:45:51 -0700

    Classify Game Boy hardware families

--- src/Craterboy.Core/GameBoyModel.cs
+++ src/Craterboy.Core/GameBoyModel.cs
@@ -18,8 +18,15 @@ public enum GameBoyModel
 
 public static class GameBoyModelExtensions
 {
+    public static bool IsCgbRevision(this GameBoyModel model) =>
+        model is >= GameBoyModel.Cgb0 and <= GameBoyModel.CgbE;
+
+    public static bool IsAgb(this GameBoyModel model) => model is GameBoyModel.AgbA;
+
+    public static bool IsGbp(this GameBoyModel model) => model is GameBoyModel.GbpA;
+
     public static bool IsColor(this GameBoyModel model) =>
-        model is >= GameBoyModel.Cgb0 and <= GameBoyModel.GbpA;
+        model.IsCgbRevision() || model.IsAgb() || model.IsGbp();
 
     public static bool IsSuperGameBoy(this GameBoyModel model) =>
         model is GameBoyModel.Sgb or GameBoyModel.Sgb2;
