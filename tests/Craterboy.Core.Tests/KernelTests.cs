79a0cb737a8def8f91787f734baa6bfeb6e3bb2d
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-17 18:44:07 -0700

    Cover CGB-D VRAM bank selection (#289)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -856,6 +856,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
