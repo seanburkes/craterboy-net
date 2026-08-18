53e29502c649339a14381086c0162b663f6678d8
Sean Burkes <sean.burkes@gmail.com>
2026-08-18 07:29:33 -0700

    Cover CGB-D WRAM bank selection

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -886,6 +886,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
