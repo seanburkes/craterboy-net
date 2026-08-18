a2f66967b22f99bd6cf413e3b5b25c0c711f11a7
Sean Burkes <sean.burkes@gmail.com>
2026-08-17 17:57:55 -0700

    Cover CGB-D blocked palette writes

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -796,6 +796,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
