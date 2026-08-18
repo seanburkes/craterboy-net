ef37ef4602c07c607a1a69e4375e5e357da5d38f
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-18 07:35:07 -0700

    Cover CGB-D KEY1 preparation (#291)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -920,6 +920,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
