0946fb405b2489889aaad1e97f1bf2833543d35f
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-19 16:19:27 -0700

    Cover CGB-D DMA high RAM sources (#319)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1747,6 +1747,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
