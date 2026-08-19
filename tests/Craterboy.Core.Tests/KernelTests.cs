6a29368eb05df3875b03e45b4319ee3f0942988b
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-19 15:10:14 -0700

    Cover CGB-D DMA echo sources (#317)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1703,6 +1703,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
