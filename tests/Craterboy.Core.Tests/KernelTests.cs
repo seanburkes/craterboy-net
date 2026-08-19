7344475532518c6775aab096be1b8ac9280c3ba6
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 11:05:25 -0700

    Cover CGB-D sprite palette rendering

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1542,6 +1542,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
