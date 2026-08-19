6ed92ecbfdbfee05f6da6827a419ad5e1130f73d
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-19 11:01:57 -0700

    Cover CGB-D background palette rendering (#308)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1463,6 +1463,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
