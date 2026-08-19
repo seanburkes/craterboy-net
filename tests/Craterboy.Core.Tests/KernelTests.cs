f19ebf1870614da0cfdf686fae2fd5b1410a8c9f
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 10:56:00 -0700

    Cover CGB-D color background priority

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1305,6 +1305,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
