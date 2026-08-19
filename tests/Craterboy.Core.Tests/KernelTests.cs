51b0944b50b1ea90c2d0f83a56cb96c6070a4968
Sean Burkes <sean.burkes@gmail.com>
2026-08-18 19:26:42 -0700

    Cover CGB-D prepared KEY1 stop

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -936,6 +936,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
