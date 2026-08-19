758a149ffc5a27ed9277f00b05b6fc254edb63ad
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 10:56:56 -0700

    Cover CGB-D sprite background priority

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1340,6 +1340,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
