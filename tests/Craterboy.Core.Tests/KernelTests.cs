e21c7bbefe4460b683a69b9ce7f1ee05aaf8283c
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 12:59:39 -0700

    Cover CGB-D general DMA VRAM bank

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1676,6 +1676,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
