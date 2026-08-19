2413f2309a0401827816b536368c6eabc70265af
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-19 06:55:23 -0700

    Cover CGB-D OPRI sprite ordering (#298)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1121,6 +1121,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
