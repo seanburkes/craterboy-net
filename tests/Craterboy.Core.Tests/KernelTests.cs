3176efd8c84bfb6b7ace6ea9861abcac1bdeafca
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 16:50:50 -0700

    Cover CGB-D DMA OAM sources

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1768,6 +1768,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
