9177df524384bf3b3bffa32b61b57e33838d5be5
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 12:58:34 -0700

    Cover CGB-D window tile attributes

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1649,6 +1649,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
