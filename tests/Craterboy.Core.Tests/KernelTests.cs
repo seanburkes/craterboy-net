faa3b7e941a3af4a5a054cd77e55a2ffdf58cecd
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 07:24:25 -0700

    Cover CGB-D sprite tile bank

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1221,6 +1221,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
