b446b880ce449f9e5973c470152735c63326744d
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-19 16:18:24 -0700

    Cover CGB-D DMA VRAM wrapping (#318)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1724,6 +1724,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
