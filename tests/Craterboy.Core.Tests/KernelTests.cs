9c0085bc97783aa80cc307496cc9d00f84524e60
Sean Burkes <sean.burkes@gmail.com>
2026-08-18 20:46:09 -0700

    Cover CGB-D double-speed OAM window

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1047,6 +1047,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
