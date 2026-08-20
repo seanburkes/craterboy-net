d6e8b61ef8833cea924e0618b9636825dcf15a0c
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-20 08:24:33 -0700

    Cover CGB-D HBlank DMA timing (#323)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1829,6 +1829,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
