7f852c36555a3affc45ab0cb7fe9a99fcb26a34d
Sean Burkes <sean.burkes@gmail.com>
2026-08-19 19:50:18 -0700

    Cover CGB-D DMA IO sources

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1789,6 +1789,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
