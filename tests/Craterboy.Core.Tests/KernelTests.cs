35444cae59c00456b505c38fb1b0ecb3303908e0
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 16:49:24 -0700

    Cover CGB-D HBlank DMA VBlank exclusion (#328)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1979,6 +1979,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
