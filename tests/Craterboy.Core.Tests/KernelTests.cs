21a909d7a8d8598519a123cc284dd916d950da70
Sean Burkes <sean.burkes@gmail.com>
2026-08-21 06:04:20 -0700

    Cover CGB-D HBlank DMA HALT wake

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -1882,6 +1882,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
