1db5ace2d64a6addf03ff05aa7e699a055b95438
Sean Burkes <sean.burkes@gmail.com>
2026-08-21 17:57:02 -0700

    Cover CGB-D APU PCM registers

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -2802,6 +2802,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
