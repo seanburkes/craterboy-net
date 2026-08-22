58776aae5aa08ed0968e1ace45003f301209d4d9
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 21:41:13 -0700

    Cover CGB-D APU phase state hashing (#350)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -4567,6 +4567,7 @@ public sealed class KernelTests
     [Theory]
     [InlineData(GameBoyModel.DmgB)]
     [InlineData(GameBoyModel.Mgb)]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
