12f70b418fdf78c44a750b244dbffee87691cd74
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 20:17:48 -0700

    Cover CGB-D boot ROM mapping hashing (#344)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -4734,6 +4734,7 @@ public sealed class KernelTests
 
     [Theory]
     [InlineData(GameBoyModel.DmgB)]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
