628cc0547b2b1f64bf50ac1944bd562f51867b7a
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 22:13:20 -0700

    Cover CGB-D memory bank state hashing (#354)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -4782,6 +4782,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
