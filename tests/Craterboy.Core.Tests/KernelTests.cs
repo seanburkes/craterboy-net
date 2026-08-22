ebde4cb60f0819ce18aeb3cfe51e73891d51c4d3
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 20:34:33 -0700

    Cover CGB-D palette state hashing (#345)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -4491,6 +4491,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
