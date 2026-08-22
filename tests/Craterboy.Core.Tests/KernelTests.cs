c17b2f305aff502b0df42737a74a0f1e6c8b8219
Sean Burkes <sean.burkes@gmail.com>
2026-08-21 19:50:32 -0700

    Cover CGB-D cartridge state hashing

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -4677,6 +4677,7 @@ public sealed class KernelTests
 
     [Theory]
     [InlineData(GameBoyModel.DmgB)]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
