57ecc7b69e81171962ab93c612614a8d6b44c497
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 18:18:56 -0700

    Cover CGB-D stereo mixer routing (#336)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -2631,6 +2631,7 @@ public sealed class KernelTests
 
     [Theory]
     [InlineData(GameBoyModel.DmgB)]
+    [InlineData(GameBoyModel.CgbD)]
     [InlineData(GameBoyModel.CgbE)]
     [InlineData(GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.GbpA)]
