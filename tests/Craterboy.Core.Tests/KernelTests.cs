7440e8783149782c3100c9d273bd68730db49c09
Sean <72944513+seanburkes@users.noreply.github.com>
2026-08-21 21:07:55 -0700

    Cover CGB-D model state hashing (#347)

--- tests/Craterboy.Core.Tests/KernelTests.cs
+++ tests/Craterboy.Core.Tests/KernelTests.cs
@@ -4758,6 +4758,7 @@ public sealed class KernelTests
     }
 
     [Theory]
+    [InlineData(GameBoyModel.CgbD, GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.CgbE, GameBoyModel.AgbA)]
     [InlineData(GameBoyModel.CgbE, GameBoyModel.GbpA)]
     [InlineData(GameBoyModel.AgbA, GameBoyModel.GbpA)]
